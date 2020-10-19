be3bd36c69d1cc08ba91bc53ae0b44d698b03130
ROSE1-K <rosekely748@gmail.com>
2020-10-19 22:03:49 +0530

    Complite Project DONE

---
+++ profile.php
@@ -0,0 +1,46 @@
+<?php include 'includes/session.php'; ?>
+<?php
+	if(!isset($_SESSION['user'])){
+		header('location: index.php');
+	}
+?>
+<?php include 'includes/header.php'; ?>
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+	<?php include 'includes/navbar.php'; ?>
+	 
+	
+  
+  	<?php include 'includes/footer.php'; ?>
+  	<?php include 'includes/profile_modal.php'; ?>
+</div>
+
+<?php include 'includes/scripts.php'; ?>
+<script>
+$(function(){
+	$(document).on('click', '.transact', function(e){
+		e.preventDefault();
+		$('#transaction').modal('show');
+		var id = $(this).data('id');
+		$.ajax({
+			type: 'POST',
+			url: 'transaction.php',
+			data: {id:id},
+			dataType: 'json',
+			success:function(response){
+				$('#date').html(response.date);
+				$('#transid').html(response.transaction);
+				$('#detail').prepend(response.list);
+				$('#total').html(response.total);
+			}
+		});
+	});
+
+	$("#transaction").on("hidden.bs.modal", function () {
+	    $('.prepend_items').remove();
+	});
+});
+</script>
+</body>
+</html>
